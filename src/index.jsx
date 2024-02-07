b2c2589e18f89f0bd0bf252881bc281e0744c211
ohMegaMat <nickblue14@yahoo.com.ar>
2024-02-06 21:25:40 -0300

    index

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+//import App from "./app/containers/App";
+import AppSealed from "./app/containers/AppSealed";
+//import Counter from "./components/counter";
+
+ReactDOM.render(<AppSealed />, document.getElementById("root"));
